f3c95e59a7a7a3e6c84ca52c011dc187c7f266e4
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2023-04-28 15:32:05 -0400

    Add Fader scripts
    + Add Fader class that allows the user to fade out several common visual and audio components such as SpriteRenderers and AudioSources

---
+++ Runtime/Fader/AudioSourceVolumeFader.cs
@@ -0,0 +1,117 @@
+/*
+File: AudioSourceVolumeFader.cs
+Author: Brandon Lyman (blyman94)
+Creation Date: 4.28.2023
+
+Description:
+This script allows the user to fade the volume of an AudioSource in and out 
+using a coroutine or an asynchronous method call.
+
+Changelog:
+V1.0 - Initial Documentation (4.28.2023)
+*/
+
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Blyman94.CommonSolutions
+{
+    /// <summary>
+    /// Allows the user to fade the volume of an AudioSource in and out using a 
+    /// coroutine or an asynchronous method call.
+    /// </summary>
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSourceVolumeFader : Fader
+    {
+        /// <summary>
+        /// The volume of this AudioSource when it is faded in.
+        /// </summary>
+        [Header("Fade Volume")]
+        [Range(0, 1)]
+        [Tooltip("The volume of this AudioSource when it is faded in.")]
+        [SerializeField] private float _volumeIn = 1;
+
+        /// <summary>
+        /// The volume of this AudioSource when it is faded out.
+        /// </summary>
+        [Range(0, 1)]
+        [Tooltip("The volume of this AudioSource when it is faded out.")]
+        [SerializeField] private float _volumeOut = 0;
+
+        /// <summary>
+        /// Attached AudioSource.
+        /// </summary>
+        private AudioSource _audioSource;
+
+        #region MonoBehaviour Methods
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+        #endregion
+
+        #region Fader Methods
+        protected override IEnumerator FadeRoutine(float fadeDuration,
+            bool fadeIn, bool dynamicFadeTime)
+        {
+            float elapsedTime = 0.0f;
+            float startVolume = _audioSource.volume;
+            float targetVolume = fadeIn ? _volumeIn : _volumeOut;
+
+            if (dynamicFadeTime)
+            {
+                elapsedTime = fadeIn ? (startVolume * fadeDuration) :
+                    (1 - startVolume) * fadeDuration;
+            }
+
+            while (elapsedTime < fadeDuration)
+            {
+                _audioSource.volume = Mathf.Lerp(startVolume, targetVolume,
+                    elapsedTime / fadeDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _audioSource.volume = targetVolume;
+
+            HandleDelegatesAndEvents(fadeIn);
+        }
+
+        protected override async Task FadeAsync(float fadeDuration, bool fadeIn,
+            bool dynamicFadeTime)
+        {
+            float elapsedTime = 0.0f;
+            float startVolume = _audioSource.volume;
+            float targetVolume = fadeIn ? _volumeIn : _volumeOut;
+
+            if (dynamicFadeTime)
+            {
+                elapsedTime = fadeIn ? (startVolume * fadeDuration) :
+                    (1 - startVolume) * fadeDuration;
+            }
+
+            while (elapsedTime < fadeDuration)
+            {
+                _audioSource.volume = Mathf.Lerp(startVolume, targetVolume,
+                    elapsedTime / fadeDuration);
+                elapsedTime += Time.deltaTime;
+                await Task.Yield();
+            }
+
+            _audioSource.volume = targetVolume;
+            HandleDelegatesAndEvents(fadeIn);
+        }
+
+        public override void InImmediate()
+        {
+            _audioSource.volume = _volumeIn;
+        }
+
+        public override void OutImmediate()
+        {
+            _audioSource.volume = _volumeOut;
+        }
+        #endregion
+    }
+}
---
+++ Runtime/Fader/SpriteRendererMaterialColorFader.cs
@@ -0,0 +1,106 @@
+/*
+File: SpriteRendererMaterialColorFader.cs
+Author: Brandon Lyman (blyman94)
+Creation Date: 4.28.2023
+
+Description:
+This script allows the user to fade the color of a SpriteRenderer's material in 
+and out using a coroutine or an asynchronous method call.
+
+Changelog:
+V1.0 - Initial Documentation (4.28.2023)
+*/
+
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Blyman94.CommonSolutions
+{
+    /// <summary>
+    /// Allows the user to fade the color of a SpriteRenderer's material in and 
+    /// out using a coroutine or an asynchronous method call.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteRendererMaterialColorFader : Fader
+    {
+        /// <summary>
+        /// The color of this SpriteRenderer's material when it is faded in.
+        /// </summary>
+        [Header("Fade Color")]
+        [Tooltip("The color of this SpriteRenderer's material when it is " + 
+            "faded in.")]
+        [SerializeField] private Color _colorIn = Color.white;
+
+        /// <summary>
+        /// The color of this SpriteRenderer's material when it is faded out.
+        /// </summary>
+        [Tooltip("The color of this SpriteRenderer's material when it is " + 
+            "faded out.")]
+        [SerializeField] private Color _colorOut = 
+            new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        /// <summary>
+        /// Attached SpriteRenderer.
+        /// </summary>
+        private SpriteRenderer _spriteRenderer;
+
+        #region MonoBehaviour Methods
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        #endregion
+
+        #region Fader Methods
+        protected override IEnumerator FadeRoutine(float fadeDuration,
+            bool fadeIn, bool dynamicFadeTime)
+        {
+            float elapsedTime = 0.0f;
+            Color startColor = _spriteRenderer.material.color;
+            Color targetColor = fadeIn ? _colorIn : _colorOut;
+
+            while (elapsedTime < fadeDuration)
+            {
+                _spriteRenderer.material.color = Color.Lerp(startColor, 
+                    targetColor, elapsedTime / fadeDuration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _spriteRenderer.material.color = targetColor;
+
+            HandleDelegatesAndEvents(fadeIn);
+        }
+
+        protected override async Task FadeAsync(float fadeDuration, bool fadeIn,
+            bool dynamicFadeTime)
+        {
+            float elapsedTime = 0.0f;
+            Color startColor = _spriteRenderer.material.color;
+            Color targetColor = fadeIn ? _colorIn : _colorOut;
+
+            while (elapsedTime < fadeDuration)
+            {
+                _spriteRenderer.material.color = Color.Lerp(startColor, 
+                    targetColor, elapsedTime / fadeDuration);
+                elapsedTime += Time.deltaTime;
+                await Task.Yield();
+            }
+
+            _spriteRenderer.material.color = targetColor;
+            HandleDelegatesAndEvents(fadeIn);
+        }
+
+        public override void InImmediate()
+        {
+            _spriteRenderer.material.color = _colorIn;
+        }
+
+        public override void OutImmediate()
+        {
+            _spriteRenderer.material.color = _colorOut;
+        }
+        #endregion
+    }
+}
