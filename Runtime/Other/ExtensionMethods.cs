93625cd973de15b57c21fbf32f6c0ac6b3184ace
Brandon Lyman <66562222+blyman94@users.noreply.github.com>
2023-09-25 22:10:14 -0400

    Update ExtensionMethods.cs

--- Runtime/Other/ExtensionMethods.cs
+++ Runtime/Other/ExtensionMethods.cs
@@ -10,46 +10,42 @@ namespace Blyman94.CommonSolutions
         public static IEnumerator FadeIn(this CanvasGroup canvasGroup,
             float fadeDuration)
         {
-            float elapsedTime = canvasGroup.alpha * fadeDuration;
-            while (elapsedTime < fadeDuration)
-            {
-                canvasGroup.alpha = Mathf.Lerp(0.0f,
-                    1.0f, elapsedTime / fadeDuration);
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-
-            canvasGroup.alpha = 1.0f;
+            yield return canvasGroup.FadeIn(fadeDuration, 1.0f, 
+                canvasGroup.blocksRaycasts, canvasGroup.interactable);
         }
         public static IEnumerator FadeIn(this CanvasGroup canvasGroup,
-            float fadeDuration, bool blocksRaycastsIn, bool interactableIn)
+            float fadeDuration, float maxAlpha)
         {
-            float elapsedTime = canvasGroup.alpha * fadeDuration;
+            yield return canvasGroup.FadeIn(fadeDuration, maxAlpha, 
+                canvasGroup.blocksRaycasts, canvasGroup.interactable);
+        }
+        public static IEnumerator FadeIn(this CanvasGroup canvasGroup,
+            float fadeDuration, float maxAlpha, bool blocksRaycastsIn, 
+            bool interactableIn)
+        {
+            if (maxAlpha <= 0.0f)
+            {
+                maxAlpha = 0.001f;
+            }
+
+            float elapsedTime = (canvasGroup.alpha / maxAlpha) * fadeDuration;
             while (elapsedTime < fadeDuration)
             {
                 canvasGroup.alpha = Mathf.Lerp(0.0f,
-                    1.0f, elapsedTime / fadeDuration);
+                    maxAlpha, elapsedTime / fadeDuration);
                 elapsedTime += Time.deltaTime;
                 yield return null;
             }
 
-            canvasGroup.alpha = 1.0f;
+            canvasGroup.alpha = maxAlpha;
             canvasGroup.blocksRaycasts = blocksRaycastsIn;
             canvasGroup.interactable = interactableIn;
         }
         public static IEnumerator FadeOut(this CanvasGroup canvasGroup,
             float fadeDuration)
         {
-            float elapsedTime = (1 - canvasGroup.alpha) * fadeDuration;
-            while (elapsedTime < fadeDuration)
-            {
-                canvasGroup.alpha = Mathf.Lerp(1.0f,
-                    0.0f, elapsedTime / fadeDuration);
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-
-            canvasGroup.alpha = 0.0f;
+            yield return canvasGroup.FadeOut(fadeDuration,
+                canvasGroup.blocksRaycasts, canvasGroup.interactable);
         }
         public static IEnumerator FadeOut(this CanvasGroup canvasGroup,
             float fadeDuration, bool blocksRaycastsOut, bool interactableOut)
